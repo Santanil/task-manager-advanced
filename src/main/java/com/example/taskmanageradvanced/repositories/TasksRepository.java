8a48d4e0bd103804d55769ba0ef9cd24f5300534
Santanil <santanilbasak@gmail.com>
2023-03-12 19:14:53 +0530

    updated Taskrepository class

--- src/main/java/com/example/taskmanageradvanced/repositories/TasksRepository.java
+++ src/main/java/com/example/taskmanageradvanced/repositories/TasksRepository.java
@@ -2,8 +2,16 @@ package com.example.taskmanageradvanced.repositories;
 
 import com.example.taskmanageradvanced.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TasksRepository extends JpaRepository<TaskEntity,Integer> {
+
+    //Example of writing a custom query -> Ideally this should be present in here
+    //rather inside service layer as this can be considered as a business logic
+    @Query("SELECT t FROM tasks t WHERE t.completed=false")
+    List<TaskEntity> findAllIncompleteTasks();
 }
