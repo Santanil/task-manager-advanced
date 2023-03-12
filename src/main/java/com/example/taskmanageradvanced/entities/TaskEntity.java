a5a186cee1babd39e102e5d4c98542aa5baf8c81
Santanil <santanilbasak@gmail.com>
2023-03-12 18:22:44 +0530

    initial commit

---
+++ src/main/java/com/example/taskmanageradvanced/entities/TaskEntity.java
@@ -0,0 +1,29 @@
+package com.example.taskmanageradvanced.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity(name="tasks")
+@Getter
+@Setter
+public class TaskEntity extends BaseEntity {
+
+    @Column(name="title", nullable = false, length = 150)
+    String title;
+
+    @Column(name="completed", nullable = false, columnDefinition = "boolean default false")
+    Boolean completed;
+
+    @Column(name="due_date")
+    Date dueDate;
+
+    //We should write following lines if we want to fetch all the notes when a task is fetched
+    //@OneToMany(mappedBy = "tasks",fetch=FetchType.EAGER)
+    //List<NoteEntity> notes;
+}
---
+++ src/test/java/com/example/taskmanageradvanced/TaskManagerAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmanageradvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
